6a2df52b7f1ce0e5cb5fb99c5b62a54aa40a8619
Kirill_divine <kirill.sokolov96@gmail.com>
2025-05-10 13:48:43 +0300

    rework starter class

--- src/product/rate/Starter.java
+++ src/product/rate/Starter.java
@@ -1,14 +1,19 @@
 package product.rate;
 
 public class Starter {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         ProductRepository repository = new ProductRepository();
-        ProductManager manager = new ProductManager();
+        InputService inputService = new InputService();
+        ProductManager productManager = new ProductManager();
 
-        manager.productDataEntry(repository);
+        inputService.fillProducts(repository);
 
-        manager.printProducts(repository);
-        manager.printTopRatedProducts(repository);
-        manager.printBestProduct(repository);
+        System.out.println("Товары с рейтингом выше 4.0:");
+        for (Product product : productManager.getTopRatedProducts(repository.getAllProducts())){
+            System.out.println(product);
+        }
+
+        System.out.println("Товар с лучшим рейтингом:");
+        System.out.println(productManager.getBestProduct(repository.getAllProducts()));
     }
 }
