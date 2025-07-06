20f5c9ffe70e836a60236ae5a2cd7b9f29b16d1f
Kirill_divine <kirill.sokolov96@gmail.com>
2025-07-06 17:51:44 +0300

    create a row sum function

--- src/arrays/SumOfElements.java
+++ src/arrays/SumOfElements.java
@@ -21,13 +21,22 @@ public class SumOfElements {
         return result;
     }
 
+    public static int findRowSum(int[] rowArray){
+        int result = 0;
+        for (int i = 0; i < rowArray.length; i++){
+            result += rowArray[i];
+        }
+
+        return result;
+    }
+
     public static int[][] fillMatrix() {
         int[][] matrix = new int[3][4];
         Random random = new Random();
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = random.nextInt(1000) + 1;
+                matrix[i][j] = random.nextInt(10) + 1;
             }
         }
 
