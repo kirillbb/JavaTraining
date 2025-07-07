2fa84fb38eb9f4f5dad38726b447dc4db7a96407
Kirill_divine <kirill.sokolov96@gmail.com>
2025-07-07 19:52:51 +0300

    added get longest word and digit count functions

--- src/stringTraining/StringTraining.java
+++ src/stringTraining/StringTraining.java
@@ -9,12 +9,41 @@ public class StringTraining {
         System.out.println("Enter the string:");
         String string = scanner.nextLine();
 
-        System.out.println("Word count:" + getWordArray(string).length);
+        String[] words = getWordArray(string);
+        System.out.println("Word count: " + words.length);
+
+        System.out.println("Longest word: " + getLongestWord(words));
+
+        System.out.println("Number of digits: " + digitCount(string));
     }
 
     public static String[] getWordArray(String inputString) {
         inputString = inputString.trim();
 
         return inputString.split("\\s+");
+    }
+
+    public static String getLongestWord(String[] words) {
+        String longestWord = words[0];
+
+        for (int i = 0; i < words.length; i++) {
+            if (longestWord.length() < words[i].length()) {
+                longestWord = words[i];
+            }
+        }
+
+        return longestWord;
+    }
+
+    public static int digitCount(String inputString) {
+        int result = 0;
+        for (int i = 0; i < inputString.length(); i++) {
+            char ch = inputString.charAt(i);
+            if (Character.isDigit(ch)) {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
