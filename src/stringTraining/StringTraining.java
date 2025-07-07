3ac6ada8b4281d754b7dd0022520ca2e7fe588c4
Kirill_divine <kirill.sokolov96@gmail.com>
2025-07-07 19:30:52 +0300

    added a simple word counting function

--- src/stringTraining/StringTraining.java
+++ src/stringTraining/StringTraining.java
@@ -1,7 +1,21 @@
 package stringTraining;
 
+import java.util.Scanner;
+
 public class StringTraining {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the string:");
+        String string = scanner.nextLine();
+
+        System.out.println("Word count:" + wordCount(string));
+    }
+
+    public static int wordCount(String inputString) {
+        inputString = inputString.trim();
+        String[] words = inputString.split("\\s+");
+
+        return words.length;
     }
 }
